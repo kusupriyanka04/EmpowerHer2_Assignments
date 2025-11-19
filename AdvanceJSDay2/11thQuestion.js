fcb6afdef35b3530ea0c1d493687114a0ab00d38
kpriyanka <kusupriyanka04@gmail.com>
2025-11-19 14:51:51 +0530

    map() and forEach()

---
+++ AdvanceJSDay2/11thQuestion.js
@@ -0,0 +1,14 @@
+function processProducts(products) {
+    const productNames = products.map(product => product.name);
+
+    products.forEach(product => {
+        const message = product.price > 50 ? "is above $50" : "is below $50";
+        console.log(`${product.name} ${message}`);
+    });
+
+    return productNames;
+}
+
+const products = [{ name: "Laptop", price: 1000 }, { name: "Mouse", price: 20 }];
+// const names = processProducts(products);
+// console.log(names); 
