68ed0e7ea2810cf87c6dd628254ecf8cf3c5aa2b
kpriyanka <kusupriyanka04@gmail.com>
2025-11-19 07:22:54 +0530

    Multi-level array and object destructuring

--- AdvanceJSDay1/ES6-features.js
+++ AdvanceJSDay1/ES6-features.js
@@ -4,31 +4,9 @@
 
 
 
-// const people = [ 
-//     { name: "Alice", address: { city: "New York", street: { name: "Broadway", number: 123 } } }, 
-//     { name: "Bob", address: { city: "Los Angeles", street: { name: "Sunset Boulevard", number: 456 } } } 
-// ];
-
-// const result = people.map(({name, address: {city, street: {name: streetName } } } ) =>
-//     `${name} lives in ${city} on ${streetName}`
-// );
-// console.log(result);
 
 
-function mergeProfileUpdates(profile, updates) {
-    return {
-        ...profile,
-        ...updates,
-        address: {
-            ...profile.address,
-            ...updates.address
-        }
-    };
-}
-const profile = {name: "riya", age: 21, address:{city: "San Francisco", zipcode: "56904"}} ;
-const updates = { age: 30, address: { zipcode: "56906", country: "USA" } };
-const mergedProfile = mergeProfileUpdates(profile, updates);
-console.log(mergedProfile);
+
 
 const userName = "Priyanka";
 const course = "JavaScript";
