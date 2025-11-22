c801095915398fbcb9869b21ebe704d05e25e473
kpriyanka <kusupriyanka04@gmail.com>
2025-11-22 12:57:09 +0530

    closure

--- AdvanceJSDay3/closure.js
+++ AdvanceJSDay3/closure.js
@@ -20,3 +20,41 @@ counter.increment();
 counter.increment();
 counter.increment();
 counter.decrement();
+
+console.log("---------------------------------")
+// Question2 : Simulating Private Variable with Closures
+
+function createBankAccount() {
+  let balance = 0;
+  let transactionHistory = [];
+
+  return {
+    deposit: function(amount) {
+      balance += amount;
+      transactionHistory.push(`Deposited: ${amount}`);
+      console.log(`Deposited: ${amount}`);
+    },
+    withdraw: function(amount) {
+      if (amount > balance) {
+        console.log("Insufficient balance");
+      } else {
+        balance -= amount;
+        transactionHistory.push(`Withdrawn: ${amount}`);
+        console.log(`Withdrawn: ${amount}`);
+      }
+    },
+    balance: function() {
+      console.log(balance);
+    },
+    transactionHistory: function() {
+      console.log(transactionHistory);
+    }
+  };
+}
+
+const account = createBankAccount();
+account.deposit(500); 
+account.withdraw(200); 
+account.withdraw(400); 
+account.balance(); 
+account.transactionHistory();
