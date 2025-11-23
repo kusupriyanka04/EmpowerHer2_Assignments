8e59639069b5d5e7cfbfc673ae5125f100b17333
kpriyanka <kusupriyanka04@gmail.com>
2025-11-23 13:29:14 +0530

    spread, rest & destructuring

--- AdvanceJSDay1/17thQuestion.js
+++ AdvanceJSDay1/17thQuestion.js
@@ -1,9 +1,10 @@
-//17(a)
+//17(a)Use spread operator to merge:
 arr1 = [1, 2, 3];
 arr2 = [4, 5];
 console.log([...arr1, ...arr2])
 
-// 17(b)
+console.log("-----------------------------");
+// 17(b)Create a function sum(...nums) using rest operator that returns the sum of all numbers.
 function add(...nums){
     let total = 0;
     for(let n of nums) total += n;
@@ -12,6 +13,7 @@ function add(...nums){
 add(18, 7, 4);
 add(18, 2);
 
+console.log("-----------------------------");
 // 17(c)
 const user = {
   username: "Alice",
