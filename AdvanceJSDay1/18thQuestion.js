da0eb38d43f2dbabb3766deb804b465b47f4eecf
kpriyanka <kusupriyanka04@gmail.com>
2025-11-23 13:16:40 +0530

    scoping & optional chaining

--- AdvanceJSDay1/18thQuestion.js
+++ AdvanceJSDay1/18thQuestion.js
@@ -5,8 +5,12 @@ if (true) {
 }
 console.log(y);
 //console.log(x);
+//Explain why:
+// y is declared with var, which has function scope. Since it's not inside any function here, y is in the global scope. So, console.log(y) prints 20.
+// x is declared with let, which has block scope. The if block is its scope. Since console.log(x) is outside this block, x is not accessible here, causing a ReferenceError.
 
-// 18
+// 18(b) optional chaining
+console.log("---------------------------------");
 const Uprofile = {
   user: {
     details: {
@@ -17,6 +21,8 @@ const Uprofile = {
 console.log(Uprofile?.user?.details?.email);
 console.log(Uprofile?.user?.details?.email?.phone);
 
+// 18(c) 
+console.log("----------------------------------");
 const employee = {
     emname : "laya",
     branch : {
