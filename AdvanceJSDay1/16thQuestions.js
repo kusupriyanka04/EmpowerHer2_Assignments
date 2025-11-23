09957b281b436ecf3e32163cf5bb77cbc235e0bf
kpriyanka <kusupriyanka04@gmail.com>
2025-11-23 13:26:10 +0530

    Arrow Function & Ternary Operator

--- AdvanceJSDay1/16thQuestions.js
+++ AdvanceJSDay1/16thQuestions.js
@@ -1,14 +1,16 @@
-// 16(a)
+// 16(a) Write an arrow function isEven(n) that returns true if even, else false.
 const isEven = n => n % 2 === 0;
 console.log(isEven(4));
 console.log(isEven(7));
 
-// 16(b)
+console.log("-----------------------------");
+// 16(b)ternary operator
 const marks = 60
 const grade = marks >= 35 ? "pass" : "fail";
-console.log(grade)
+console.log(grade);
 
-//16(c)
+console.log("-----------------------------");
+//16(c)Write an arrow function greet(name) that prints: "Hello, " If no name is passed, print "Guest" using the ternary operator.
 const greet = (name) => console.log(`Hello, ${name ? name: "guest"}
     `);
     greet("sandya");
